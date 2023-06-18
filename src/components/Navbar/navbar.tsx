1516f5f690f88ebee33c46785f680a6a8a3d789f
shinoshina <1982600638@qq.com>
2023-06-18 22:23:53 +0800

    feat: proto生成ts api

---
+++ src/components/Navbar/navbar.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+
+interface LinkTabProps {
+  label?: string;
+  href?: string;
+}
+
+function LinkTab(props: LinkTabProps) {
+  return (
+    <Tab
+      component="a"
+      onClick={(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        event.preventDefault();
+      }}
+      {...props}
+    />
+  );
+}
+
+export default function NavTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Tabs value={value} onChange={handleChange} aria-label="nav tabs example">
+            <LinkTab label="Page One" href="/drafts" />
+            <LinkTab label="Page Two" href="/trash" />
+            <LinkTab label="Page Three" href="/spam" />
+            <Button variant="text">Text</Button>
+          </Tabs>
+        </Grid>
+        <Grid item xs={4}>
+          <Button>nihao</Button>
+        </Grid>
+      </Grid>
+    </Box>
+
+  );
+}
