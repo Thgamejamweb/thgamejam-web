f6beda6d6af8653a7ad4b512c23a7a5fb0152175
zhiguai <2635435377@qq.com>
2023-06-26 21:34:22 +0800

    🌈 style:

--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -1,13 +1,16 @@
 import { Box, Grid, Stack, Typography } from "@mui/material";
 import NavBar from "../../component/navbar";
 import Bottombar from "../../component/bottombar";
+import React from "react";
 
 
 export default function Home() {
     return (
         <>
             <NavBar></NavBar>
-            <Box sx={{height:window.innerHeight - 60 }}></Box>
+            <Box sx={{height:window.innerHeight - 60 }}>
+                
+            </Box>
             <Bottombar></Bottombar>
         </>
     )
