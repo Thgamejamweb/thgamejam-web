7480b88959e250b6e3413ac5b0bd02e0851ef09f
zhiguai <2635435377@qq.com>
2023-07-07 00:49:36 +0800

    🐞 fix:

--- src/component/navbar.tsx
+++ src/component/navbar.tsx
@@ -6,6 +6,7 @@ import Grid from '@mui/material/Grid';
 import LinkButton from './linkbutton';
 import { useNavigate } from 'react-router-dom';
 // import { useHistory } from 'react-router-dom';
+import { userApi } from "@/http/http_api";
 
 interface LinkTabProps {
     label?: string;
@@ -28,13 +29,20 @@ function LinkTab(props: LinkTabProps) {
 
 export default function NavBar() {
     const [value, setValue] = React.useState(0);
+    const [status, setStatus] = React.useState(0);
 
     const handleChange = (event: React.SyntheticEvent, newValue: number) => {
         setValue(newValue);
 
     };
 
+    userApi.getUserTokenInfo(undefined).then(req => {
+        setStatus(1);
+    })
+    
     return (
+        status == 0
+        ?
         <Box sx={{ width: '100%', height: 60,borderBottom:1,borderColor:'#DADADA' }}>
             <Grid container>
                 <Grid item xs={2}>
@@ -42,9 +50,7 @@ export default function NavBar() {
                 </Grid>
                 <Grid item xs={6} >
                     <Tabs sx={{ height: 60 }} value={value} onChange={handleChange} >
-                        <LinkTab label="upload game" href="/drafts" />
-                        <LinkTab label="gamejams" href="/trash" />
-                        <LinkTab label="user team" href="/user/index" />
+                        <LinkTab label="首页" href="/index" />
                     </Tabs>
                 </Grid>
                 <Grid item xs={4} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} >
@@ -53,6 +59,22 @@ export default function NavBar() {
                 </Grid>
             </Grid>
         </Box >
-
+        :
+        <Box sx={{ width: '100%', height: 60,borderBottom:1,borderColor:'#DADADA' }}>
+            <Grid container>
+                <Grid item xs={2}>
+                    logo
+                </Grid>
+                <Grid item xs={6} >
+                    <Tabs sx={{ height: 60 }} value={value} onChange={handleChange} >
+                        <LinkTab label="首页" href="/index" />
+                        <LinkTab label="队伍管理" href="/user/team" />
+                    </Tabs>
+                </Grid>
+                <Grid item xs={4} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }} >
+                    <LinkButton name='用户中心' href='/user'></LinkButton>
+                </Grid>
+            </Grid>
+        </Box >
     );
 }
