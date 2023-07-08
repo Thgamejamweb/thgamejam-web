d22a7929a74cbd6a1b00bf0a2b1edd6a418336e5
zhiguai <2635435377@qq.com>
2023-07-08 13:35:26 +0800

    ✨ feat:

--- src/page/user/workEdit.tsx
+++ src/page/user/workEdit.tsx
@@ -85,7 +85,7 @@ export default function Home() {
     const addWorks = () => {
         workApi.updateWorks(new UpdateWorksRequest({
             name: name,
-            worksId:workId,
+            worksId: workId,
             teamId: teamId,
             headerImageURL: headerImageURL,
             content: content,
@@ -471,6 +471,19 @@ export default function Home() {
                                     'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
                                     'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
                                     'insertdatetime', 'media', 'table', 'help', 'wordcount'
+                                ],
+                                textpattern_patterns: [
+                                    { start: '*', end: '*', format: 'italic' },
+                                    { start: '**', end: '**', format: 'bold' },
+                                    { start: '#', format: 'h1' },
+                                    { start: '##', format: 'h2' },
+                                    { start: '###', format: 'h3' },
+                                    { start: '####', format: 'h4' },
+                                    { start: '#####', format: 'h5' },
+                                    { start: '######', format: 'h6' },
+                                    { start: '1. ', cmd: 'InsertOrderedList' },
+                                    { start: '* ', cmd: 'InsertUnorderedList' },
+                                    { start: '- ', cmd: 'InsertUnorderedList' }
                                 ],
                                 toolbar: 'undo redo | blocks | ' +
                                     'bold italic backcolor | alignleft aligncenter ' +
